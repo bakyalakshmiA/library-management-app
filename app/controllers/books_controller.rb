3dec1d2e8c520100f384f0f520b21d4e45db4025
bakyalakshmiA <bakyalakshmi.angappan1@freshworks.com>
2024-09-09 01:35:46 +0530

    books controller bugs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     end
 
     def create
-        book_params = params.require(:books).permit(
+        book_params = params.require(:book).permit(
           :book_id,
           :title,
           :author,
@@ -35,6 +35,12 @@ class BooksController < ApplicationController
         book_ids = params.require(:book_ids)
         books = Book.where(id: book_ids)
         @member.books << books
+        render json: { message: "Books borrowed successfully" }, status: :ok
+    end
+
+    def borrowed_books
+        member = Member.find(params.require(:member_id))
+        book_ids = member.borrowed_books.pluck(:book_id)
         render json: { message: 'Books borrowed successfully' , books: "#{@books}" }, status: :ok
     end
 
